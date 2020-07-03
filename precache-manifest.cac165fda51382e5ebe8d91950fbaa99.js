5304d10d67b1b01d8db0ea9cebb9fb0ff5fb0daa
StackiCode <stacki.code@gmail.com>
2020-07-03 11:09:38 +0700

    Updates

---
+++ precache-manifest.cac165fda51382e5ebe8d91950fbaa99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c17f4b56da0a354e67c56271b21fcc3",
+    "url": "/cardFlip/index.html"
+  },
+  {
+    "revision": "494a510c412ac63050c3",
+    "url": "/cardFlip/static/css/main.76271018.chunk.css"
+  },
+  {
+    "revision": "348cae6730aaf2657bfc",
+    "url": "/cardFlip/static/js/2.73701f37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cardFlip/static/js/2.73701f37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "494a510c412ac63050c3",
+    "url": "/cardFlip/static/js/main.4eec0ce0.chunk.js"
+  },
+  {
+    "revision": "46955b8881dd7739c62d",
+    "url": "/cardFlip/static/js/runtime-main.5666f6e3.js"
+  }
+]);
